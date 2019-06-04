54b50969003a26d9a76cb210b10b8a2c1d871d27
ANTON\A <masjanin@yandex.ru>
2019-06-04 16:12:01 +0300

    fixbug enable pipeline on start from bat file

--- irr.py
+++ irr.py
@@ -1,9 +1,11 @@
 import scrapy
 from scrapy.crawler import CrawlerProcess
 from abrds.spiders.irr import IrrSpider
+from abrds.spiders.cmlt import CmltSpider
+from scrapy.utils.project import get_project_settings
 
-if __name__ == '__main__':
-    process = CrawlerProcess()
-
-    process.crawl(IrrSpider)
-    process.start() # the script will block here until the crawling is finished
+settings = get_project_settings()
+process = CrawlerProcess(settings)
+process.crawl(IrrSpider)
+# process.crawl(CmltSpider)
+process.start() # the script will block here until the crawling is finished
